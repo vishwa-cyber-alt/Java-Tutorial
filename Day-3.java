089df63d717b6dbdc8e1171cccbe19ed62ea776d
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-07-20 21:30:03 +0530

    Day 3

---
+++ Day-3.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+
+public class Main
+
+{
+
+public static void main(String args[])
+
+{
+
+HashSet<String> a=new HashSet<String>();
+
+a.add("windows");
+
+a.add("linux");
+
+a.add("mac");
+
+System.out.println(a.contains("linux"));
+
+System.out.print(a.contains("kernal"));
+
+
+
+}}

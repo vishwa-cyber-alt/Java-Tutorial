1c59401964042e1d5174279397b9c30f0cff415d
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-07-27 16:34:04 +0530

    Create How many years and month need to settle the loan.java

---
+++ How many years and month need to settle the loan.java
@@ -0,0 +1,12 @@
+public class Loan
+{
+    public static void main(String args[])
+    {
+        int s=30000;
+        int l=1000000;
+        float m=l/s;
+        System.out.println("total month="+m);
+       float y=m/12;
+       System.out.println("total year="+y);
+    }
+}

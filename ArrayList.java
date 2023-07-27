834841c93014248a354e84bb8ed3205f8837815c
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-07-27 14:40:01 +0530

    Create ArrayList.java

---
+++ ArrayList.java
@@ -0,0 +1,12 @@
+
+import java.util.ArrayList;
+class Al 
+{
+    public static void main(String[] args)
+   {
+        ArrayList<String> car = new ArrayList<String>();
+        car.add("one");
+        car.add("hyandai");
+        System.out.print(car);
+    }
+}

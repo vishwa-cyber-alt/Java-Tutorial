a96ea652165d24634473e82792910fc8923a5fc1
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-07-28 14:24:51 +0530

    Create Finally.java

---
+++ Finally.java
@@ -0,0 +1,15 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class July282023 {
+    public static void main(String[] args) {
+        
+        try{
+            int a[]={1,2};
+            System.out.println(a[0]);
+    }
+    catch(Exception e){System.out.println("no element not there");
+}
+        finally{System.out.println("finished");
+    }
+}

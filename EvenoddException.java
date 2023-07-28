dcf5cd154bb306b531eeba34d622dd8a35f716d2
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-07-28 21:19:38 +0530

    Create EvenoddException.java

---
+++ EvenoddException.java
@@ -0,0 +1,19 @@
+class Main
+  {
+public static void main(String args[])
+    {
+try
+  {
+int n=132;
+    if(n%2==0)
+    {
+System.out.print("even");
+    }
+    else{
+throw new Exception();}}
+  catch(Exception e)
+    {
+    System.out.print("odd");
+  }
+  }
+  }

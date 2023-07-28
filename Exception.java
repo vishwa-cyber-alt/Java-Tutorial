bc9d22b211d3c48c7dc3fc69974a02cb35abeaa2
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-07-28 13:59:05 +0530

    Create Exception.java

---
+++ Exception.java
@@ -0,0 +1,15 @@
+
+
+class July282023
+  {
+    public static void main(String[] args)
+    {
+        
+        try
+          {
+            int a[]={1,2};
+            System.out.print(a[0]);
+          }
+    catch(Exception e){System.out.print("no element not there");
+}
+  }

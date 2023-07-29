ff249a3aa060c5646a1a5fee9d0677ac18c4484a
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-07-29 20:05:15 +0530

    Create createobject.java

---
+++ createobject.java
@@ -0,0 +1,12 @@
+class Main
+{
+    String name="qwe";
+    int empid=21;
+    public static void main(String[] args)
+    {
+      //o represent object
+       Main o=new Main();
+       System.out.print(o.name);
+         System.out.print(o.empid);
+    }
+}

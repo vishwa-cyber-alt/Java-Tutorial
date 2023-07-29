2bb323dd3154b1a8c1acdc8506b18362e7d5425a
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2023-07-29 22:20:58 +0530

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,7 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int a[]={1,6,8,9,0};
+        for(int i=0;i<a.length;i++){System.out.println(i+"st element="+a[i]);
+    }
+}

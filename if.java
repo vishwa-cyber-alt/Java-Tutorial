12adf056e1059a8240a3dd8c74c8e51e4a5a146b
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2024-01-10 10:54:53 +0530

    Create if.java

---
+++ if.java
@@ -0,0 +1,13 @@
+class Main
+{
+public static void main(String args[])
+{
+int pas=12;
+System.out.print("enter reg no=");
+if(pas!=12)
+{
+System.out.print("ok");
+}
+else{
+System.out.print("no");
+}}}

868669e828350cd1e275cbb8c00b283148dd8beb
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2024-02-08 13:29:07 +0530

    Create Mul_method.java

---
+++ Mul_method.java
@@ -0,0 +1,15 @@
+class One
+{
+    void add()
+    {
+        int a=28;
+        int b=41;
+        int c=a*b;
+        System.out.print(c);
+    }
+    public static void main(String args[])
+    {
+        One caller=new One();
+        caller.add();
+    }
+}

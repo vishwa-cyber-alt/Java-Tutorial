2265e6ca836d18f76240bbf48567a31911972391
vishwa-cyber-alt <60313865+vishwa-cyber-alt@users.noreply.github.com>
2025-03-16 19:48:44 +0530

    Create equals.java

---
+++ equals.java
@@ -0,0 +1,15 @@
+class Main
+{
+    public static void main(String[] args)
+    {
+        String a="Vishwa";
+        String b="Github";
+        String c="Vishwa";
+        Check(a,b);
+        Check(a,c);
+    }
+        public static void Check(String s1,String s2)
+        {            
+        System.out.println(s1.equals(s2));   
+        }
+}
